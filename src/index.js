e03e8bf751da85d57c22245d73740b9776fc1c5a
Rafael Teixeira <rafael.amtx@gmail.com>
2019-09-12 01:28:46 -0300

    react logo

--- src/index.js
+++ src/index.js
@@ -4,9 +4,12 @@ import './index.css';
 import Calculator from './main/Calculator';
 import * as serviceWorker from './serviceWorker';
 
+import reactlogo from './imgs/logo.svg'
+
 ReactDOM.render(
 
 <div>
+<img src={reactlogo} alt="logo" className="react-logo"/>
 <h1>Calculadora</h1>
 <Calculator />
 </div>
